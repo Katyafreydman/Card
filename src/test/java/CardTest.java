fe80b940b3de9a1db757f26654e88fad91f1c0ed
Катя Фрейдман <katyafreydman@gmail.com>
2022-11-22 14:13:45 +0300

    Added test

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -10,4 +10,42 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class CardTest {
+    private WebDriver driver;
 
+    @BeforeAll
+    static void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void BeforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldPositiveRequest ()  {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("Екатерина Иванова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79818939922");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expectedMessage = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+
+
+}
